39e42ce1d1bf9948e1dd17594863fd74f23b9267
Philip <powerphillg5@gmail.com>
2022-06-06 19:09:23 -0700

    :truck: refactor command-manager into its own folder

--- bin/scripts/create-response-handler/index.js
+++ bin/scripts/create-response-handler/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const CommandManager = require("../../utils/command-manager");
+const CommandManager = require("../../utils/command-manager/command-manager");
 const config = require("./config");
 
 new CommandManager("./commands", config).execute();
