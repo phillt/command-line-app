34f284d2a7f64aa53f60be5f062a14d125454495
Philip <powerphillg5@gmail.com>
2022-06-07 13:37:46 -0700

    :truck: add salt path maps

---
+++ bin/salt-path-maps.js
@@ -0,0 +1,7 @@
+const path_maps = Object.freeze({
+	"response-handler-tests-factory":
+		"/resources/js/pepper/test-utils/response-handler-tests-factory.js",
+	"mock-hal-class": "/resources/js/pepper/test-utils/mock-hal-class.js",
+});
+
+module.exports = path_maps;
