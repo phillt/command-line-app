0ed68fca1b5a245e303af366e077f90c87bb84a5
Philip <powerphillg5@gmail.com>
2022-06-15 13:35:42 -0700

    :building_construction: don't give the responsibility of naming files to the template manager.

--- bin/utils/template-manager/template-manager.js
+++ bin/utils/template-manager/template-manager.js
@@ -1,45 +1,24 @@
 const fs = require("fs");
-const TextFilters = require("../text-filters/text-filters");
-const HumanApi = require("../human-api");
 
 class TemplateManager {
-	name;
 	#interpolated_templates = [];
-	#interpolation_question_map = [];
 	templates = [];
 	write_path = "./";
 	interpolation_data = null;
 
-	constructor({ interpolation_data, templates, interpolation_question_map, write_path = "./" }) {
+	constructor({ interpolation_data, templates, write_path = "./" }) {
 		this.interpolation_data = interpolation_data;
 		this.templates = templates;
 		this.write_path = write_path;
-
-		this.#interpolation_question_map = [
-			...this.#interpolation_question_map,
-			...interpolation_question_map,
-		];
-	}
-
-	/**
-	 * @deprecated - use buildTemplates
-	 */
-	buildComponent() {
-		this.requestFileName();
-		this.gatherInterpolationData(this.#interpolation_question_map);
-		this.interpolateTemplates();
-		this.writeComponentFile();
-		console.log("Done dude!");
 	}
 
 	buildTemplates() {
-		this.gatherInterpolationData(this.#interpolation_question_map);
 		this.interpolateTemplates();
 		return this.getInterpolatedTemplates();
 	}
 
 	interpolateTemplates() {
-		this.templates.forEach(({ template, ...data }) => {
+		this.templates.forEach(({ template, name, ...data }) => {
 			let interpolated_template;
 			if (typeof template === "function") {
 				interpolated_template = template(this.interpolation_data);
@@ -48,30 +27,16 @@ class TemplateManager {
 					`Expected template to be a function. Got ${typeof template} instead.`
 				);
 			}
-			this.#interpolated_templates.push({ template: interpolated_template, ...data });
+			this.#interpolated_templates.push({
+				template: interpolated_template,
+				file_name: `${name}`,
+				...data,
+			});
 		});
 	}
 
 	getInterpolatedTemplates() {
 		return this.#interpolated_templates;
-	}
-
-	/**
-	 * @deprecated - Use PathsBuddy.writeFiles() to write the templates. Use getInterpolatedTemplates to get the templates.
-	 */
-	writeComponentFile() {
-		fs.mkdirSync(`${this.write_path}${this.dash_name}`);
-		this.#interpolated_templates.forEach(({ template, extension }) => {
-			fs.writeFile(
-				`${this.write_path}${this.dash_name}/${this.dash_name}${extension}`,
-				template.trim(),
-				function (err) {
-					if (err) {
-						return console.log("Something went wrong creating the file", err);
-					}
-				}
-			);
-		});
 	}
 }
 
