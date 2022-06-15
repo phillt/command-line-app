e7961f97f4df299ce662ef9d66d0e3eaab12cdf5
Philip <powerphillg5@gmail.com>
2022-06-15 13:36:21 -0700

    :tada: testing building tools for slots now.

--- bin/scripts/create-component/utils/test-building-tools.js
+++ bin/scripts/create-component/utils/test-building-tools.js
@@ -1,15 +1,52 @@
 class TestBuildingTools {
-	static buildDefaultOptions(injection) {
-		if (!injection) {
-			return "";
+	static buildDefaultOptions({ injection = [], slots = [] }) {
+		const options = {};
+
+		if (injection?.length) {
+			const injectOptions = {};
+			injection.forEach(
+				({ injection, sample_injection }) => (injectOptions[injection] = sample_injection)
+			);
+
+			options.provide = injectOptions;
 		}
 
-		const injectOptions = {};
-		injection.forEach(
-			({ injection, sample_injection }) => (injectOptions[injection] = sample_injection)
-		);
+		if (slots?.length) {
+			let slot_options = {};
+			slots.forEach(({ name }) => {
+				slot_options[name] = `content for ${name}`;
+			});
 
-		return `const default_options = {provide: ${JSON.stringify(injectOptions)}};`;
+			options.slots = slot_options;
+		}
+
+		return `${JSON.stringify(options)}`;
+	}
+
+	static buildSlotTests(wrapper_component, slots) {
+		let slot_yields = ``;
+		let slot_options = {};
+		slots.forEach(({ name }) => {
+			slot_yields += `
+			yield ["${name}", "content for ${name}"];`;
+			slot_options[name] = "content for ${name}";
+		});
+
+		return `
+			describe("Renders slots correctly", function () {
+				const provideSlotContent = function *(){
+					${slot_yields}
+				}
+				
+				const sut = shallowMount(${wrapper_component}, default_options);
+				for (const conditions of provideSlotContent()) {
+					const [slot_name, expected_content] = conditions;
+					it(slot_name, function () {
+					    assert.contains(sut.html(), expected_content);
+					});				
+				}
+			});
+		`;
 	}
 }
 
