fa5538265ff761b47979065d0d0790a54a555e01
Philip <powerphillg5@gmail.com>
2022-06-15 13:36:35 -0700

    :alien: update api for slots

--- bin/scripts/create-component/commands/pepper-component/component-test-template.js
+++ bin/scripts/create-component/commands/pepper-component/component-test-template.js
@@ -1,6 +1,6 @@
 const TestBuildingTools = require("../../utils/test-building-tools");
 const TextFilter = require("../../../../utils/text-filters/text-filters");
-const component_test_template = function ({ name, injection }) {
+const component_test_template = function ({ name, injection, slots }) {
 	const dash_name = TextFilter.toDashCase(name);
 	const camel_name = TextFilter.toCamelCase(name);
 
@@ -9,13 +9,15 @@ import ${camel_name} from "./${dash_name}";
 import { shallowMount } from "@vue/test-utils";
 import { assert } from "@sinonjs/referee";
 
-${TestBuildingTools.buildDefaultOptions(injection)}
+const default_options = ${TestBuildingTools.buildDefaultOptions({ injection, slots })};
 describe("${name} tests.", function () {
 	it("renders", function () {
 			assert(shallowMount(${camel_name}${
 		injection ? ", default_options" : ""
 	}).find({ref: "${dash_name}"}).exists());
 	});
+	
+	${TestBuildingTools.buildSlotTests(camel_name, slots)}
 });
 `;
 };
