837e5a65093cd8181a168e46e57e171d37a25453
Philip <powerphillg5@gmail.com>
2022-07-12 17:11:14 -0700

    :construction: more hal stuff

--- bin/scripts/create-response-handler/commands/hal/command.js
+++ bin/scripts/create-response-handler/commands/hal/command.js
@@ -10,11 +10,62 @@ const createHalMixin = function () {
 	humanApi.survey([
 		{
 			key: "name",
-			question: "Name of this mixin?",
+			question: "file name (use dash for spaces IE: my-response). no extension.",
 		},
 		{
 			key: "default_url",
 			question: "What is the default URL the api should call?",
+		},
+		{
+			key: "request_methods",
+			optional: "Add request type?",
+			continue_question: "Add another?",
+			collection: [
+				{
+					key: "type",
+					question: "What kind of request?",
+					choices: ["fetch", "post", "delete", "patch", "put"],
+				},
+				{
+					key: "name",
+					question: "request name? (camel case IE myRequestMethod)",
+				},
+			],
+		},
+		{
+			key: "raw_response",
+			optional: "Import a raw response?",
+			continue_question: "Add another?",
+			object: [
+				{
+					key: "import",
+					question:
+						"variable we will be importing. If not default, please include curly braces: { my_var } ",
+				},
+				{
+					key: "from",
+					question: "from path, please don\t include quotes: ie path/to/file.js",
+				},
+			],
+		},
+		{
+			key: "computed_props",
+			optional: "Map computed properties (dont' include response) ?",
+			continue_question: "Add another?",
+			collection: [
+				{
+					key: "key",
+					question: "computed name?",
+				},
+				{
+					key: "default_value",
+					question: "Default value?",
+				},
+				{
+					key: "mapped_response",
+					question: "Value when mapped with raw response?",
+				},
+			],
 		},
 	]);
 
