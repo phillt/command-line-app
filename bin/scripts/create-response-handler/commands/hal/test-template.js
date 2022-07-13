a495158f0417f19476ec04c5c1e3a3cf5c162dd1
Philip <powerphillg5@gmail.com>
2022-07-12 17:11:59 -0700

    :construction: more work in progress

--- bin/scripts/create-response-handler/commands/hal/test-template.js
+++ bin/scripts/create-response-handler/commands/hal/test-template.js
@@ -1,50 +1,19 @@
-const TextFilter = require("../../../../utils/text-filters/text-filters");
-const PathsBuddy = require("../../../../utils/paths-buddy/paths-buddy");
+const naming_helper = require("./naming_helper");
+const TestTemplateTools = require("../../utils/test-template-tools");
 
-const testTemplate = function ({ name, default_url }) {
-	const dash_name = TextFilter.toDashCase(name);
-	const camel_name = TextFilter.toCamelCase(name);
-	const snake_name = TextFilter.toSnakeCase(name);
-
-	const api_namespace = snake_name + "_api";
-	const api_response = snake_name + "_response";
-	const mixin_import = camel_name;
+const testTemplate = function (template_variables) {
+	const { raw_response, dash_name, camel_name, response_name, default_url, computed_props } =
+		naming_helper(template_variables);
 
 	return `
-import ${mixin_import} from "./${dash_name}";
-// Update to correct path!
-import {
-    validateComputedDataWithResponse,
-    validateDefaultApiComputedData,
-    validateDefaultApiStateData,
-    validateFetchMethod,
-    validateFetchMethodHandlesErrors,
-} from "${PathsBuddy.getRelativePathTo("response-handler-tests-factory")}";
+import ${camel_name} from "./${dash_name}";
+${TestTemplateTools.importResponseHandlerTestsFactory()}
+${TestTemplateTools.buildRawResponseImport(raw_response)}
 
-describe("${name} tests", function () {
-    validateDefaultApiStateData(${mixin_import}, "${api_namespace}", "${default_url}");
-
-    const provideExpectedComputedValues = function* () {
-        yield ["${api_response}", null];
-    };
-
-    validateDefaultApiComputedData(${mixin_import}, provideExpectedComputedValues);
-
-	// @todo validate year with response
-    // validateComputedDataWithResponse();
-
-    const provideFetchMethodArgumentsAndExpectedUrl = function* () {
-        yield ["${TextFilter.toCamelCase(`fetch ${name}`)}", [], "${default_url}"];
-        yield ["${TextFilter.toCamelCase(
-			`fetch ${name}`
-		)}", ["with/arbitrary/url"], "with/arbitrary/url"];
-    };
-
-    validateFetchMethod(${mixin_import}, "${api_namespace}", provideFetchMethodArgumentsAndExpectedUrl);
-
-    validateFetchMethodHandlesErrors(${mixin_import}, "${api_namespace}", "${TextFilter.toCamelCase(
-		`fetch ${name}`
-	)}");
+describe("${dash_name} tests", function () {
+    validateDefaultApiStateData(${camel_name}, "${camel_name}", "${default_url}");
+   
+	${TestTemplateTools.buildValidateDefaultComputedData(response_name, camel_name, computed_props)}
 });
 `;
 };
